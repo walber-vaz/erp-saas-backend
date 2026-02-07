680401c1bfa115614a02c6267b129c56ff5791d5
Walber Vaz da Silva <wvs.walber@gmail.com>
2026-02-07 20:57:29 -0300

    feat: integrate Prisma with PostgreSQL and add necessary configurations
    - Updated package.json to include Prisma dependencies and scripts for migration and generation.
    - Created prisma.config.ts for Prisma configuration with environment variable support.
    - Added schema.prisma to define the database schema and generator settings.
    - Implemented PrismaService to manage database connections and lifecycle hooks in NestJS.
    - Updated tsconfig.build.json to include prisma.config.ts in the build process.

---
+++ src/prisma.service.ts
@@ -0,0 +1,32 @@
+import {
+  Injectable,
+  type OnModuleDestroy,
+  type OnModuleInit,
+} from '@nestjs/common';
+import { PrismaClient } from './generated/prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
+    super({
+      adapter,
+      log:
+        process.env.NODE_ENV === 'development' ? ['query', 'error'] : ['error'],
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
